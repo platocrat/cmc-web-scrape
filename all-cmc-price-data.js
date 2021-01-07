702c0670d902f06f0d97bd762f7cc54c0eda064e
Jack Winfield <platocrat@tuta.io>
2021-01-06 22:58:05 -0600

    Uncommenting out to test

--- all-cmc-price-data.js
+++ all-cmc-price-data.js
@@ -9,12 +9,12 @@ const url = 'https://coinmarketcap.com/'
  * @todo Automate the process of selecting/getting the coin name and 
  * ticker symbol.
  */
-// cmcScript.getAndSaveData(url, puppeteer, fs, "bitcoin", "btc-usd") // BTC/USD
+cmcScript.getAndSaveData(url, puppeteer, fs, "bitcoin", "btc-usd") // BTC/USD
 cmcScript.getAndSaveData(url, puppeteer, fs, "ethereum", "eth-usd") // ETH/USD
-// cmcScript.getAndSaveData(url, puppeteer, fs, "bitcoin-cash", "bch-usd") // BCH/USD
-// cmcScript.getAndSaveData(url, puppeteer, fs, "polkadot-new", "dot-usd") // DOT/USD
-// cmcScript.getAndSaveData(url, puppeteer, fs, "binance-coin", "bnb-usd") // BNB/USD
-// cmcScript.getAndSaveData(url, puppeteer, fs, "chainlink", "link-usd") // LINK/USD
-// cmcScript.getAndSaveData(url, puppeteer, fs, "usd-coin", "usdc-usd") // USDC/USD
-// cmcScript.getAndSaveData(url, puppeteer, fs, "uniswap", "uni-usd") // UNI/USD
-// cmcScript.getAndSaveData(url, puppeteer, fs, "aave", "aave-usd") // AAVE/USD
+cmcScript.getAndSaveData(url, puppeteer, fs, "bitcoin-cash", "bch-usd") // BCH/USD
+cmcScript.getAndSaveData(url, puppeteer, fs, "polkadot-new", "dot-usd") // DOT/USD
+cmcScript.getAndSaveData(url, puppeteer, fs, "binance-coin", "bnb-usd") // BNB/USD
+cmcScript.getAndSaveData(url, puppeteer, fs, "chainlink", "link-usd") // LINK/USD
+cmcScript.getAndSaveData(url, puppeteer, fs, "usd-coin", "usdc-usd") // USDC/USD
+cmcScript.getAndSaveData(url, puppeteer, fs, "uniswap", "uni-usd") // UNI/USD
+cmcScript.getAndSaveData(url, puppeteer, fs, "aave", "aave-usd") // AAVE/USD
