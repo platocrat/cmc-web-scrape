1892a6f996c60a05df9be9741c44de940ba0f07e
Jack Winfield <platocrat@tuta.io>
2021-01-07 21:49:07 -0600

    Added DeFi and Storage, YF will take some time

--- coinmarketcap-scripts/index.js
+++ coinmarketcap-scripts/index.js
@@ -2,14 +2,22 @@ const puppeteer = require('puppeteer')
 const fs = require('fs')
 const getAllCryptos = require('./getAllCryptos')
 const getAllDeFi = require('./getAllDeFi')
-const getAllStorage = require('./getAllStorage')
+const getAllStorage = require('./getAllYieldFarming')
 
-const url = 'https://coinmarketcap.com/'
+const cryptoURL = 'https://coinmarketcap.com/'
+const defiURL = 'https://coinmarketcap.com/defi'
+const storageURL = 'https://coinmarketcap.com/storage'
+// const yieldFarmingURL = 'https://coinmarketcap.com/yield-farming'
 
 /**
  * @dev Get historical price data for each of CMC's "Cryptocurrencies", "DeFi",
  * and "Storage" categories.
  */
-getAllCryptos.getAndSaveCryptocurrenciesData(url, puppeteer, fs)
-getAllDeFi.getAndSaveDeFiData(url, puppeteer, fs)
-getAllStorage.getAndSaveStorageData(url, puppeteer, fs)
+getAllCryptos.getAndSaveData(cryptoURL, puppeteer, fs)
+getAllCryptos.getAndSaveData(defiURL, puppeteer, fs)
+getAllCryptos.getAndSaveData(storageURL, puppeteer, fs)
+
+/**
+ * @todo Yield farming has subcategories I need to deal with
+ */
+// getAllCryptos.getAndSaveData(yieldFarmingURL, puppeteer, fs)
