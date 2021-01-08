3b2831d961711bed69bf1b64c188aa1b0f7b9510
Jack Winfield <platocrat@tuta.io>
2021-01-07 21:01:05 -0600

    cleaned CMC script names

---
+++ coinmarketcap-scripts/index.js
@@ -0,0 +1,13 @@
+const puppeteer = require('puppeteer')
+const fs = require('fs')
+const getAllCryptos = require('./getAllCryptos')
+const getAllDeFi = require('./getAllDeFi')
+
+const url = 'https://coinmarketcap.com/'
+
+/**
+ * @dev Get historical price data for each of CMC's "Cryptocurrencies", "DeFi",
+ * and "Storage" categories.
+ */
+getAllCryptos.getAndSaveCryptocurrenciesData(url, puppeteer, fs)
+getAllDeFi.getAndSaveDeFiData(url, puppeteer, fs)
